9daacc4b2382cc903882854911ee40d4885b2952
Tayatakoi <saifmamaev720@gmail.com>
2025-11-25 23:45:59 +0400

    Add files via upload

---
+++ server_bonus.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server_bonus.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: samamaev <samamaev@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/25 23:12:57 by samamaev          #+#    #+#             */
+/*   Updated: 2025/11/25 23:43:08 by samamaev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk_bonus.h"
+
+unsigned char	g_building_char = 0;
+
+void	ft_putnbr(int n)
+{
+	char	c;
+
+	if (n >= 10)
+		ft_putnbr(n / 10);
+	c = (n % 10) + '0';
+	write(1, &c, 1);
+}
+
+void	signal_handler(int sig, siginfo_t *info, void *context)
+{
+	static int	bit_count = 0;
+
+	(void)context;
+	g_building_char = g_building_char << 1;
+	if (sig == SIGUSR2)
+		g_building_char = g_building_char | 1;
+	bit_count++;
+	if (bit_count == 8)
+	{
+		write(1, &g_building_char, 1);
+		bit_count = 0;
+		g_building_char = 0;
+	}
+	usleep(100);
+	kill(info->si_pid, SIGUSR1);
+}
+
+int	main(void)
+{
+	struct sigaction	sa;
+
+	write(1, "Server PID: ", 12);
+	ft_putnbr(getpid());
+	write(1, "\n", 1);
+	sa.sa_sigaction = signal_handler;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	while (1)
+		pause();
+	return (0);
+}
